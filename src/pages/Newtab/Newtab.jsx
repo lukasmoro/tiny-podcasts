76fea2cc8b954dc0c2897fd87d195d7f32a75fe4
lukasmoro <97121073+lukasmoro@users.noreply.github.com>
2022-09-04 16:02:54 +0100

    minor changes

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -1,13 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './Newtab.css';
 
-//NEXT STEPS
-//Loading exactly five feeds at the same time
-//Only load first item
-//Get enclosure tag
-//Format title
-//UI
-
 const Newtab = () => {
   const [items, setItems] = useState([]);
   const formatRss = async (newUrl) => {
@@ -30,13 +23,9 @@ const Newtab = () => {
   useEffect(() => {
     chrome.storage.local.get(['newUrl'], (item) => {
       formatRss(item.newUrl);
+      console.log(item.newUrl);
     });
   }, []);
-
-  // useEffect(() => {
-  //   console.log(rssUrl);
-  //   formatRss(rssUrl);
-  // }, [rssUrl]);
 
   return (
     <div className="App">
@@ -54,41 +43,3 @@ const Newtab = () => {
 };
 
 export default Newtab;
-
-/*
-export default function List({ rssUrl }) {
-  const [items, setItems] = useState([]);
-  const formatRss = async (rssUrl) => {
-    console.log(rssUrl);
-    const res = await fetch(`https://api.allorigins.win/get?url=${rssUrl}`);
-    const { contents } = await res.json();
-    const feed = new window.DOMParser().parseFromString(contents, 'text/xml');
-    const items = feed.querySelectorAll('item');
-    const feedItems = [...items].map((el) => ({
-      link: el.querySelector('link').innerHTML,
-      title: el.querySelector('title').innerHTML,
-      author: el.querySelector('author').innerHTML,
-    }));
-    setItems(feedItems);
-  };
-
-  useEffect(() => {
-    console.log(rssUrl);
-    formatRss(rssUrl);
-  }, [rssUrl]);
-
-  return (
-    <div className="App">
-      {items.map((item) => {
-        return (
-          <div>
-            <h1>{item.title}</h1>
-            <p>{item.author}</p>
-            <a href={item.link}>{item.link}</a>
-          </div>
-        );
-      })}
-    </div>
-  );
-}
-*/
--- src/pages/Options/Options.jsx
+++ src/pages/Options/Options.jsx
@@ -1,14 +1,9 @@
-import React, { useState } from 'react';
-import { render } from 'react-dom';
-import Search from './Search';
-import Tester from './Tester';
+import React from 'react';
+import Tester from './Tester4';
 
 export default function Options() {
-  // // const [rssUrl, setRssUrl] = useState(['']);
-
   return (
     <div className="App">
-      {/* <Search rssUrl={rssUrl} setRssUrl={setRssUrl} /> */}
       <Tester />
     </div>
   );
