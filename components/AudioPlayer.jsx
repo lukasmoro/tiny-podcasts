8384347650fbd7d2b6aabecfb3f96ffbd2d488d9
lukasmoro <lukas.moro@me.com>
2023-08-31 15:58:26 +0200

    audioplayer done

--- components/AudioPlayer.jsx
+++ components/AudioPlayer.jsx
@@ -5,34 +5,45 @@ const AudioPlayer = () => {
 
     //integrate data from chrome.storage
     //style audioplayer
-    //animate audioplayer using react spring
+    //animate progressbar using react spring
     //store currentTime variable in chrome.storage
 
     const [isPlaying, setIsPlaying] = useState(false);
     const [duration, setDuration] = useState(0);
     const [currentTime, setCurrentTime] = useState(0);
 
+    const audioPlayer = useRef();
+    const progressBar = useRef();
+    const animationRef = useRef();
+
     const togglePlayPause = () => {
         const prevValue = isPlaying;
         setIsPlaying(!prevValue);
-        if (prevValue) {
+        if (!prevValue) {
             audioPlayer.current.play();
+            animationRef.current = requestAnimationFrame(whilePlaying)
         } else {
             audioPlayer.current.pause();
+            cancelAnimationFrame(animationRef.current);
         }
     }
 
-    const audioPlayer = useRef();
+    const whilePlaying = () => {
+        progressBar.current.value = audioPlayer.current.currentTime;
+        changePlayerCurrentTime();
+        animationRef.current = requestAnimationFrame(whilePlaying);
+    }
 
     const onLoadedMetadata = () => {
         const seconds = Math.floor(audioPlayer.current.duration);
         setDuration(seconds);
+        progressBar.current.max = seconds;
     };
 
-    // useEffect(() => {
-    //     const seconds = Math.floor(audioPlayer.current.duration);
-    //     setDuration(seconds);
-    // }, [audioPlayer?.current?.loadedmetadata, audioPlayer?.current?.readyState])
+    const changeRange = () => {
+        audioPlayer.current.currentTime = progressBar.current.value;
+        changePlayerCurrentTime();
+    }
 
     const calculateTime = (secs) => {
         const minutes = Math.floor(secs / 60);
@@ -40,8 +51,11 @@ const AudioPlayer = () => {
         const seconds = secs % 60;
         const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
         return `${returnedMinutes}:${returnedSeconds}`
+    }
 
-
+    const changePlayerCurrentTime = () => {
+        progressBar.current.style.setProperty('--seek-before-width', `${progressBar.current.value / duration * 100}%`);
+        setCurrentTime(progressBar.current.value);
     }
 
     return (
@@ -52,7 +66,7 @@ const AudioPlayer = () => {
             <button className='play-pause' onClick={togglePlayPause}>{isPlaying ? <p>play</p> : <p>pause</p>}</button>
             <button className='forward-backward'>front 30</button>
             <div className='current-time'>{calculateTime(currentTime)}</div>
-            <div><input type="range" /></div>
+            <div><input className='progress-bar' type="range" defaultValue="0" ref={progressBar} onChange={changeRange} /></div>
             {/* add logic for saving current duration to chrome.storage.local and then playing from there the next time */}
             <div className='duration' >{(duration && !isNaN(duration)) && calculateTime(duration)}</div>
         </div >
