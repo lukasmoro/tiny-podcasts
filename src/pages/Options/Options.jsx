ff3af61f02fa0af797c50b24c632c6cf460f94bc
Lukas Moro <lukas.moro@me.com>
2025-03-03 12:57:58 +0100

    bento options

--- src/pages/Options/Options.jsx
+++ src/pages/Options/Options.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import List from './List';
+import Overlay from '../Newtab/Overlay';
 import { ThemeProvider } from '../Newtab/ThemeProvider';
 import './Options.css';
 import './Form.css';
@@ -9,17 +10,50 @@ export default function Options() {
   return (
     <div className="App">
       <ThemeProvider>
+        <Overlay />
         <div className="spacer"></div>
-        <div className="card">
-          <div className="container">
-            <h2 className="sub-header">Managing Podcasts</h2>
-            <h1 className="header">Options</h1>
-            <p className="instructions">
-              Manage podcasts here or in the pop-up window.
-            </p>
-            <List />
-            <p className="greets">Enjoy your Podcasts! 🎧</p>
+        <div className="cards-container">
+          <div className="card left-card">
+            <div className="container">
+              <h2 className="sub-header">Managing Podcasts</h2>
+              <h1 className="header">Options</h1>
+              <p className="instructions">
+                Manage podcasts here or in the pop-up window.
+              </p>
+              <div className="overflow">
+                <List />
+              </div>
+              <p className="greets">Enjoy your Podcasts! 🎧</p>
+            </div>
           </div>
+
+          <div className="right-cards">
+            <div className="card right-card">
+              <div className="container">
+                <h2 className="sub-header">Finding Podcasts</h2>
+                <h1 className="header">Recommendations</h1>
+                <p className="instructions">
+                  Unsure where to start? Here are some favorites.
+                </p>
+                <div className="items"></div>
+              </div>
+            </div>
+
+            <div className="card right-card">
+              <div className="container">
+                <h2 className="sub-header">Support Podcasts</h2>
+                <h1 className="header">Feedback & Donation</h1>
+                <p className="instructions">
+                  Enjoy podcasts? Please consider supporting. This will help
+                  creating more tiny products in the future.
+                </p>
+                <div className="items">
+                  <p></p>
+                </div>
+              </div>
+            </div>
+          </div>
+
           <p className="signature">
             Podcasts by <a href="https://lukasmoro.com">Lukas Moro</a>.
             <br />
