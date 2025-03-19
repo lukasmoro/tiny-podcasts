298df6394d53d6ff8d464cf6fe944a53ac28d165
Lukas Moro <lukas.moro@me.com>
2025-03-19 18:34:29 +0100

    onboarding animation

--- src/pages/Icons/ArrowIcon.jsx
+++ src/pages/Icons/ArrowIcon.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 export const ArrowIcon = ({ className = '', onClick }) => (
-  <svg width="87" height="107" viewBox="0 0 87 107" fill="none" xmlns="http://www.w3.org/2000/svg" className={className} onClick={onClick}>
+  <svg width="87" height="107" viewBox="0 0 87 107" fill="none" xmlns="http://www.w3.org/2000/svg" className={`arrow-animation ${className}`} onClick={onClick}>
     <g>
-      <path d="M1 106C24.9068 101.883 46.485 88.8901 61.5958 69.5127C76.7067 50.1353 84.2863 25.7379 82.8809 1" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-      <path d="M80.0479 6.35314L86.0001 6.18434L82.8809 1L80.0479 6.35314Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path className="arrow-path" d="M1 106C24.9068 101.883 46.485 88.8901 61.5958 69.5127C76.7067 50.1353 84.2863 25.7379 82.8809 1" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path className="arrow-tip" d="M80.0479 6.35314L86.0001 6.18434L82.8809 1L80.0479 6.35314Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
     </g>
   </svg>
 ); 
